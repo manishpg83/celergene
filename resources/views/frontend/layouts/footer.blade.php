c24a081f89637c561b281c700216ef92a40ed73c
manishpg83 <manish.bhuvait@gmail.com>
2025-01-10 10:18:26 +0530

    Design Work Done

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,62 +1,62 @@
 <section class="footer" style="font-size: 12px;">
-    <div class="container-fluid bg-grey px-3 px-lg-0 py-5">
-       <div class="container">
-          <div class="row">
-             <div class="col-lg-4 AdelleSansRegular text-lg-start text-center order-2 order-lg-1">
-                <h5 class="text-blue mb-3"> CONTACT US </h5>
-                <p class="text-darkgrey ">
-                   EMAIL: <span class="ps-lg-3 ps-0"> marketing@celergenswiss.com </span>
-                </p>
-                <div class="row">
-                   <div class="col-lg-1 pe-lg-5 pe-0">
-                      <p class="text-darkgrey mb-0">OFFICE: </p>
-                   </div>
-                   <div class="col-lg-7 ps-lg-4 ps-0">
-                      <p class="text-darkgrey mb-0">
-                         Celergen SA, 2C Rue Nicolas Bové, L-1253 Luxembourg
-                      </p>
-                      <p class="text-darkgrey mb-0">Grand Duchy of Luxembourg</p>
-                   </div>
-                </div>
-             </div>
-             <div class="col-lg-8 text-lg-start text-center AdelleSansRegular order-1 order-lg-2">
-                <h5 class="text-blue mb-3"> SIGN UP FOR OUR NEWSLETTER </h5>
-                <form class="form-group">
-                  <div class="row">
-                     <div class="col-lg-6 mb-3">
-                        <input type="email" class="border-0 rounded-0 px-2 py-3 w-100" placeholder="Your Email" required="">
-                     </div>
-                     <div class="col-lg-3">
-                        <input type="submit" value="SUBSCRIBE" class="btn red-btn rounded-0 bg-darkred py-3 w-100 border-0 text-white">
-                     </div>
-                     <div class="col-lg-3 d-flex align-items-center p-3 justify-content-lg-start justify-content-center mb-md-4">
-                        <div class="">
-                           <a class="me-4  d-inline" href="https://www.facebook.com/CelergenSwiss" target="_blank"><img src="https://cdn2-bread6hkcwg3dyar.z01.azurefd.net/celergenswiss/images/common/ic_fb-gray.png" alt=""></a>
-                           <a class="me-4  d-inline" href="https://www.instagram.com/celergeneurope" target="_blank"><img src="https://cdn2-bread6hkcwg3dyar.z01.azurefd.net/celergenswiss/images/common/ic_instagram_gray.png" alt=""></a>
-                           <a class="d-inline" href="https://twitter.com/CelergenSocial" target="_blank"><img src="https://cdn2-bread6hkcwg3dyar.z01.azurefd.net/celergenswiss/images/common/ic_twitter-gray.png" alt=""></a>
-                        </div>
-                     </div>
+   <div class="container-fluid bg-grey px-3 px-lg-0 py-5">
+      <div class="container">
+         <div class="row">
+            <div class="col-lg-4 AdelleSansRegular text-lg-start text-center order-2 order-lg-1">
+               <h5 class="text-blue mb-3"> CONTACT US </h5>
+               <p class="text-darkgrey ">
+                  EMAIL: <span class="ps-lg-3 ps-0"> marketing@celergenswiss.com </span>
+               </p>
+               <div class="row">
+                  <div class="col-lg-1 pe-lg-5 pe-0">
+                     <p class="text-darkgrey mb-0">OFFICE: </p>
                   </div>
-               </form>
-             </div>
-          </div>
-       </div>
-    </div>
-    <div class="container-fluid py-4">
-       <div class="container-lg px-0 px-lg-2">
-          <div class="row">
-             <div class="col-xl-7 col-lg-6 mt-3 mt-lg-0 text-center text-lg-start order-2 order-lg-1">
-                <p class="m-0 d-none d-lg-block">© Celergen International Limited. All Rights Reserved.</p>
-                <p class="m-0 d-block d-lg-none">© Celergen SA. All Rights Reserved.</p>
-             </div>
-             <div class="col-xl-5 col-lg-6 text-lg-end text-center order-1 order-lg-2">
-                <div class="row">
-                   <div class="col-lg-4"><a href="privacy-policy.php" class="py-1 text-dark footer-link">PRIVACY POLICY</a></div>
-                   <div class="col-lg-3"><a href="authenticity.php" class="py-1 text-dark footer-link">AUTHENTICITY</a></div>
-                   <div class="col-lg-5"><a href="terms-and-condition.php" class="py-1 text-dark footer-link">TERMS AND CONDITIONS</a></div>
-                </div>
-             </div>
-          </div>
-       </div>
-    </div>
- </section>
+                  <div class="col-lg-7 ps-lg-4 ps-0">
+                     <p class="text-darkgrey mb-0">
+                        Celergen SA, 2C Rue Nicolas Bové, L-1253 Luxembourg
+                     </p>
+                     <p class="text-darkgrey mb-0">Grand Duchy of Luxembourg</p>
+                  </div>
+               </div>
+            </div>
+            <div class="col-lg-8 text-lg-start text-center AdelleSansRegular order-1 order-lg-2">
+               <h5 class="text-blue mb-3"> SIGN UP FOR OUR NEWSLETTER </h5>
+               <form class="form-group">
+                 <div class="row">
+                    <div class="col-lg-6 mb-3">
+                       <input type="email" class="border-0 rounded-0 px-2 py-3 w-100" placeholder="Your Email" required="">
+                    </div>
+                    <div class="col-lg-3">
+                       <input type="submit" value="SUBSCRIBE" class="btn red-btn rounded-0 bg-darkred py-3 w-100 border-0 text-white">
+                    </div>
+                    <div class="col-lg-3 d-flex align-items-center p-3 justify-content-lg-start justify-content-center mb-md-4">
+                       <div class="">
+                          <a class="me-4  d-inline" href="https://www.facebook.com/CelergenSwiss" target="_blank"><img src="https://cdn2-bread6hkcwg3dyar.z01.azurefd.net/celergenswiss/images/common/ic_fb-gray.png" alt=""></a>
+                          <a class="me-4  d-inline" href="https://www.instagram.com/celergeneurope" target="_blank"><img src="https://cdn2-bread6hkcwg3dyar.z01.azurefd.net/celergenswiss/images/common/ic_instagram_gray.png" alt=""></a>
+                          <a class="d-inline" href="https://twitter.com/CelergenSocial" target="_blank"><img src="https://cdn2-bread6hkcwg3dyar.z01.azurefd.net/celergenswiss/images/common/ic_twitter-gray.png" alt=""></a>
+                       </div>
+                    </div>
+                 </div>
+              </form>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="container-fluid py-4 footer-bottom">
+      <div class="container-lg px-0 px-lg-2">
+         <div class="row">
+            <div class="col-xl-7 col-lg-6 mt-3 mt-lg-0 text-center text-lg-start order-2 order-lg-1">
+               <p class="m-0 d-none d-lg-block">© Celergen International Limited. All Rights Reserved.</p>
+               <p class="m-0 d-block d-lg-none">© Celergen SA. All Rights Reserved.</p>
+            </div>
+            <div class="col-xl-5 col-lg-6 text-lg-end text-center order-1 order-lg-2">
+               <div class="row">
+                  <div class="col-lg-4"><a href="privacy-policy.php" class="py-1 text-dark footer-link">PRIVACY POLICY</a></div>
+                  <div class="col-lg-3"><a href="authenticity.php" class="py-1 text-dark footer-link">AUTHENTICITY</a></div>
+                  <div class="col-lg-5"><a href="terms-and-condition.php" class="py-1 text-dark footer-link">TERMS AND CONDITIONS</a></div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
