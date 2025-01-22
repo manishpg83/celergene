75aa69bf8f1823b89c61ac8c6fca3cad0952b4ed
manishpg83 <manish.bhuvait@gmail.com>
2025-01-22 15:33:17 +0530

    added migration file for customers -> billing and billing

---
+++ database/migrations/2025_01_22_100101_add_shipping_columns_to_customers_table.php
@@ -0,0 +1,70 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->string('shipping_address_receiver_lname_1')->nullable()->after('shipping_address_receiver_name_1');
+            $table->string('shipping_address_1_1')->nullable()->after('shipping_address_1');
+            $table->string('shipping_city_1')->nullable()->after('shipping_address_1_1');
+            $table->string('shipping_state_1')->nullable()->after('shipping_city_1');
+            $table->string('shipping_phone_1', 50)->nullable()->after('shipping_state_1');
+            $table->string('shipping_email_1')->nullable()->after('shipping_phone_1');
+
+            $table->string('shipping_address_receiver_lname_2')->nullable()->after('shipping_address_receiver_name_2');
+            $table->string('shipping_address_2_1')->nullable()->after('shipping_address_2');
+            $table->string('shipping_city_2')->nullable()->after('shipping_address_2_1');
+            $table->string('shipping_state_2')->nullable()->after('shipping_city_2');
+            $table->string('shipping_phone_2', 50)->nullable()->after('shipping_state_2');
+            $table->string('shipping_email_2')->nullable()->after('shipping_phone_2');
+
+            $table->string('shipping_address_receiver_lname_3')->nullable()->after('shipping_address_receiver_name_3');
+            $table->string('shipping_address_3_1')->nullable()->after('shipping_address_3');
+            $table->string('shipping_city_3')->nullable()->after('shipping_address_3_1');
+            $table->string('shipping_state_3')->nullable()->after('shipping_city_3');
+            $table->string('shipping_phone_3', 50)->nullable()->after('shipping_state_3');
+            $table->string('shipping_email_3')->nullable()->after('shipping_phone_3');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->dropColumn([
+                'shipping_address_receiver_lname_1',
+                'shipping_address_1_1',
+                'shipping_city_1',
+                'shipping_state_1',
+                'shipping_phone_1',
+                'shipping_email_1',
+                'shipping_address_receiver_lname_2',
+                'shipping_address_2_1',
+                'shipping_city_2',
+                'shipping_state_2',
+                'shipping_phone_2',
+                'shipping_email_2',
+                'shipping_address_receiver_lname_3',
+                'shipping_address_3_1',
+                'shipping_city_3',
+                'shipping_state_3',
+                'shipping_phone_3',
+                'shipping_email_3',
+            ]);
+        });
+    }
+};
---
+++ database/migrations/2025_01_22_100206_add_billing_columns_to_customers_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->string('billing_fname', 255)->nullable()->after('billing_address');
+            $table->string('billing_lname', 255)->nullable()->after('billing_fname');
+            $table->string('billing_address_2', 255)->nullable()->after('billing_lname');
+            $table->string('billing_city', 255)->nullable()->after('billing_address_2');
+            $table->string('billing_state', 255)->nullable()->after('billing_city');
+            $table->string('billing_phone', 20)->nullable()->after('billing_state');
+            $table->string('billing_email', 255)->nullable()->after('billing_phone');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->dropColumn([
+                'billing_fname',
+                'billing_lname',
+                'billing_address_2',
+                'billing_city',
+                'billing_state',
+                'billing_phone',
+                'billing_email',
+            ]);
+        });
+    }
+};
